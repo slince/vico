02d6b20a4402a3e7e7b7a517a5af1b3df3f6c8c7
slince <taosikai@yeah.net>
2026-06-14 23:14:22 +0800

    init

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import { router } from './router';
 import './i18n';
 import './index.css';
@@ -15,7 +16,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
